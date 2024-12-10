85070307592fd17ea8e023d4dad78c9779fb7dce
Dagmawi-Y <135310799+Dagmawi-Y@users.noreply.github.com>
2024-12-10 00:46:41 +0000

    Deploying to gh-pages from @ Dagmawi-Y/portfolio-v1@ec80acbad11b3420912278730ec85e181f59692a 🚀

---
+++ _app/immutable/chunks/base.DWJC4mW5.js
@@ -0,0 +1,1 @@
+const a="Dagmawi",t="Yohannes",e={firstName:a,lastName:t,get fullName(){return`${a} ${t}`}};export{e as B};
---
+++ _app/immutable/entry/start.DSE-dxL-.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.p-QlkpWC.js";export{t as start};
