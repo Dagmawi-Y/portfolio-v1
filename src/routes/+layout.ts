ea49cda59ecb71aa3b97ad5f7c0b89bc8ba28898
Dagmawi-Y <dagmawi.inbox@gmail.com>
2024-12-10 16:29:54 +0300

    add vercel speed analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,9 @@
 import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
- 
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+
 injectAnalytics({ mode: dev ? 'development' : 'production' });
+injectSpeedInsights();
 
 export const ssr = false;
